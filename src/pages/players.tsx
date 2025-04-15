f79eb0cb28fed1cc5eb87abae5c22d99d6d6e772
João Pedro Soeiro Lemos <joaopedro@tec-adm.com.br>
2025-04-15 16:17:58 -0300

    last

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -6,11 +6,27 @@ import { getAllPlayers } from "../services/teamService";
 interface Player {
   id: number;
   name: string;
+  age: number;
+  position: string;
+  team: string;
+  image?: string;
+  careerStats: {
+    PPG: number;
+    RPG: number;
+    APG: number;
+    SPG: number;
+    BPG: number;
+    FG: number;
+    FG3: number;
+    FT: number;
+  };
 }
 
 const Players: React.FC = () => {
   const [players, setPlayers] = useState<Player[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     getAllPlayers()
@@ -22,9 +38,21 @@ const Players: React.FC = () => {
       });
   }, []);
 
-  const filteredPlayers: Player[] = players.filter((player) =>
+  const filteredPlayers = players.filter((player) =>
     player.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentPlayers = filteredPlayers.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+  const totalPages = Math.ceil(filteredPlayers.length / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
@@ -39,19 +67,37 @@ const Players: React.FC = () => {
             type="text"
             placeholder="Escolha ou pesquise um jogador"
             value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+              setCurrentPage(1);
+            }}
           />
         </div>
       </form>
+
       <div className="cards">
-        {filteredPlayers.length > 0 ? (
-          filteredPlayers.map((player) => (
+        {currentPlayers.length > 0 ? (
+          currentPlayers.map((player) => (
             <PlayerCard key={player.id} players={player} />
           ))
         ) : (
           <h2>Nenhum jogador encontrado.</h2>
         )}
       </div>
+
+      {totalPages > 1 && (
+        <div className="pagination">
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className={currentPage === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      )}
     </>
   );
 };
